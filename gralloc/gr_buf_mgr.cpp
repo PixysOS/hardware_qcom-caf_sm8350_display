AOSPA/android_hardware_qcom_display

gralloc: Extend reserved_size guard to a63c3159
Change-Id: I1f2e636c2fc6cbdc5a38506b7c11d460819d4b71

gralloc/gr_buf_mgr.cpp
@@ -856,7 +856,12 @@ void BufferManager::RegisterHandleLocked(const private_handle_t *hnd, int ion_ha
 
   if (hnd->base_metadata) {
 #ifdef METADATA_V2
+#ifdef GRALLOC_HANDLE_HAS_NO_RESERVED_SIZE
+    auto metadata = reinterpret_cast<MetaData_t *>(hnd->base_metadata);
+    buffer->reserved_size = metadata->reservedSize;
+#else
     buffer->reserved_size = hnd->reserved_size;
+#endif
     if (buffer->reserved_size > 0) {
       buffer->reserved_region_ptr =
           reinterpret_cast<void *>(hnd->base_metadata + sizeof(MetaData_t));

PixysOS/hardware_qcom-caf_sm8350_display

Signed-off-by: aswin7469 <aswinas@pixysos.com>

gralloc/gr_buf_mgr.cpp
@@ -823,7 +823,13 @@ void BufferManager::RegisterHandleLocked(const private_handle_t *hnd, int ion_ha
 
   if (hnd->base_metadata) {
 #ifdef METADATA_V2
+#ifdef GRALLOC_HANDLE_HAS_RESERVED_SIZE
     buffer->reserved_size = hnd->reserved_size;
+#else
+    auto metadata = reinterpret_cast<MetaData_t *>(hnd->base_metadata);
+    buffer->reserved_size = metadata->reservedSize;
+
+#endif
     if (buffer->reserved_size > 0) {
       buffer->reserved_region_ptr =
           reinterpret_cast<void *>(hnd->base_metadata + sizeof(MetaData_t));